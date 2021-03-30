a41a34f01bca79169b8faccf6e4c55aa381c4ca3
Brett Fitzpatrick <bfitzpatrick28@gmail.com>
2021-03-30 13:45:55 -0600

    made schema and first 2 tables

---
+++ schema.sql
@@ -0,0 +1,5 @@
+DROP DATABASE IF EXISTS employee_db;
+
+CREATE DATABASE employee_db;
+
+USE employee_db;
--- tracker.js
+++ tracker.js
@@ -1,11 +1,12 @@
 //dependiencies
 const mysql = require('mysql');
+const inquirer = require('inquirer');
 
 //make connection
 const connection = mysql.createConnection({
   host: 'localhost',
 
-  port: 8080,
+  port: 3306,
 
   user: 'root',
 
@@ -19,3 +20,18 @@ connection.connect((err) => {
   console.log(`connected as id ${connection.threadId}\n`);
   runTracker();
 });
+
+//basic prompt
+function runTracker() {
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        message: 'looks like this is working',
+        name: 'success'
+      }
+    ])
+    .then((response) => {
+      console.log('success')
+    })
+}
