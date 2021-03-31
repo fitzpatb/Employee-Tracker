b353bbc273f806707643209d655c892d0cbd19ad
Brett Fitzpatrick <bfitzpatrick28@gmail.com>
2021-03-30 19:39:10 -0600

    working on add employee function

--- schema.sql
+++ schema.sql
@@ -4,13 +4,13 @@ CREATE DATABASE employee_db;
 
 USE employee_db;
 
--- make department table --
+/*make department table */
 CREATE TABLE department (
   id INT PRIMARY KEY AUTO_INCREMENT,
   name VARCHAR(30) NOT NULL
 );
 
---make role table --
+/* make role table */
 CREATE TABLE role (
   id INT PRIMARY KEY AUTO_INCREMENT,
   title VARCHAR(30) NOT NULL,
@@ -19,7 +19,7 @@ CREATE TABLE role (
   FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
 
---make employee table --
+/*make employee table */
 CREATE TABLE employee (
   id INT PRIMARY KEY AUTO_INCREMENT,
   first_name VARCHAR(30) NOT NULL,
--- tracker.js
+++ tracker.js
@@ -26,12 +26,47 @@ function runTracker() {
   inquirer
     .prompt([
       {
-        type: 'input',
-        message: 'looks like this is working',
-        name: 'success'
+        type: 'list',
+        message: 'Select Action',
+        name: 'action',
+        choices: [
+          "Add Employee",
+          "Add Role",
+          "Add Department",
+
+        ]
       }
     ])
     .then((response) => {
-      console.log('success')
+      switch (response.action) {
+        case "Add Employee":
+          addEmployee();
+          break;
+        case "Add Role":
+          addRole();
+          break;
+        case "Add Department":
+          addDepartment();
+          break;
+      }
+    });
+}
+
+function addEmployee () {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is the Employee's first name?",
+        name: "first"
+      },
+      {
+        type: "input",
+        message: "What is the Employee's last name?",
+        name: "last"
+      }
+    ])
+    .then((response) => {
+
     })
 }
